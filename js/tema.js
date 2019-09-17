69b7cbdf4eb48330d93e0a1fc5dc8239b8bda3a9
EuclidesDoRosario <euclidesdorm@gmail.com>
2019-09-17 11:48:19 +0200

    dia 17

---
+++ js/tema.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("tema");
+const tema = window.localStorage.getItem("tema");
+
+if (tema === "dark") document.body.classList.add("dark");
+
+
+button.addEventListener("click", () => { 
+
+    document.body.classList.toggle("dark");
+
+    if (tema === "dark") {
+        window.localStorage.setItem("tema", "light");
+    } else {
+        window.localStorage.setItem("tema", "dark");
+    }
+
+});
